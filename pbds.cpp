ad4565145c4630902fb33bfba6a50f258b013188
canhnam357 <73202312+canhnam357@users.noreply.github.com>
2024-04-28 00:13:52 +0700

    Create pbds.cpp

---
+++ pbds.cpp
@@ -0,0 +1,8 @@
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+typedef tree<int, null_type, less_equal<int>, rb_tree_tag, tree_order_statistics_node_update> ordered_multiset; // like multiset
+.insert(x); // insert x
+.order_of_key(val + 1) - .order_of_key(val) > 0; // check val exists
+.erase(.find_by_order(.order_of_key(c))) // erase c , should check exists before
+.find_by_oder(pos) // s[pos] : 0-indexed
+.order_of_key(x) // n.o element less than x
