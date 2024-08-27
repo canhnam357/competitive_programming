13ca96e0cbfc8a8da32b056a872bf654ec58abbe
Canh Nam <canhnam357@gmail.com>
2024-08-28 00:25:39 +0700

    update 2dFenwick

--- 2dFenwick.cpp
+++ 2dFenwick.cpp
@@ -1,15 +1,16 @@
+template <typename T>
 struct FenWick2D {
 	int n, m;
-	vector<vector<int>> bit;
+	vector<vector<T>> bit;
 	FenWick2D() {}
 	FenWick2D(int n, int m)
 	{
 		this->n = n;
 		this->m = m;
-		bit.resize(n + 5, vector<int>(m + 5));
+		bit.resize(n + 1, vector<T>(m + 1));
 	}
 	// 1 - indexed
-	void update(int row, int col, int value) 
+	void add(int row, int col, T value) 
 	{
 		for (int i = row; i <= n; i += i & -i)
 		{
@@ -20,9 +21,9 @@ struct FenWick2D {
 		}
 	}
 	// sum of sub-rectangle [[1, 1], [row, col]]
-	int get(int row, int col)
+	T get(int row, int col)
 	{
-		int sum = 0;
+		T sum = 0;
 		for (int i = row; i; i -= i & -i)
 		{
 			for (int j = col; j; j -= j & -j)
@@ -32,8 +33,10 @@ struct FenWick2D {
 		}
 		return sum;
 	}
-	int get(int x1, int y1, int x2, int y2)
+	T get(int x1, int y1, int x2, int y2)
 	{
+		assert(1 <= x1 && x1 <= x2 && x2 <= n);
+		assert(1 <= y1 && y1 <= y2 && y2 <= m);
 		return get(x2, y2) - get(x2, y1 - 1) - get(x1 - 1, y2) + get(x1 - 1, y1 - 1);
 	}
 };
